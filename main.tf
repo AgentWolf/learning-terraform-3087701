2db51d29d37b4d621ba38001dfa9fcc3692633c4
AgentWolf <tylercoc@gmail.com>
2026-03-09 14:23:25 -0600

    ingress

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "blog_sg" {
 
   vpc_id = data.aws_vpc.default.id
   ingress_rules = ["http-80-tcp", "http-8080-tcp"]
-  ingres_cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
