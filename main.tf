d09529de21b2f90afab70cecc1e19105b77dd1bc
AgentWolf <tylercoc@gmail.com>
2026-03-12 10:15:27 -0600

    load balancer

--- main.tf
+++ main.tf
@@ -64,3 +64,41 @@ resource "aws_security_group" "blog" {
 
   vpc_id = module.blog_vpc.vpc_id
 }
+
+module "blog_alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+
+  security_groups = [module.blog_sg.security_group_id]
+
+  listeners = {
+    blog-http = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_arn = aws_lb_target_group.blog.arn
+      }
+    }
+  }
+
+  tags = {
+    Environment   = "dev"
+    terraform     = "true"
+  }
+}
+
+resource "aws_lb_target_group" "blog" {
+  name     = "blog-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.blog_vpc.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "blog" {
+  target_group_arn = aws_lb_target_group.blog.arn
+  target_id        = aws_instance.blog.arn
+  port             = 80
+}
