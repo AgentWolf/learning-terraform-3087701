d133cfd68f95e0e71e7dbbfe15366b420b9f551c
AgentWolf <tylercoc@gmail.com>
2026-03-13 09:00:13 -0600

    Variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,34 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.micro"
 }
+
+variable "ami_filter" {
+  description = "Name filter and owner for ami"
+  type = object ({
+    name  = string
+    owner = string
+
+  })
+}
+
+variable "Environment" {
+  description = "Development Environment"
+  type = object {(
+    name           = string
+    network_prefix = string
+  )}
+  default = {
+    name           = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "min_size"{
+  description = "Min number of instances in ASG
+  default = 1
+}
+
+variable "max_size"{
+  description = "Max number of instances in ASG
+  default = 2
+}
